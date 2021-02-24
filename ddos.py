36d34fc3b1d41fa4646c75ea94578ad395747b81
Ali <71032234+AliBaroot@users.noreply.github.com>
2021-02-24 15:52:01 +0330

    new_ddos.py
    خیلی چیزا رو عوض کردم
    امیدوارم خوشت بیاد

--- ddos.py
+++ ddos.py
@@ -38,22 +38,28 @@ Baner = '''\033[1;34m
 print(Baner)
 ip = input("\033[0m Please enter the IP  : ")
 port = int(input("\033[0m Please enter the Port  : "))
-os.system("clear") ; print(Baner) ; print(" \n")
-print("\033[1;33m<<===[: (please with) :]===>>")
+os.system("clear") ; print(Baner + '\n\033[1;33m<<===[: (please with) :]===>>') # <-----
 time.sleep(5)
 os.system("clear")
-print("\033[1;35m >>>> attack has been start in \n")
-time.sleep(1)
-print("\033[1;32m[: 5 :]")
-time.sleep(1)
-print("\033[1;34m[: 4 :]")
-time.sleep(1)
-print("\033[1;31m[: 3 :]")
-time.sleep(1)
-print("\033[1;39m[: 2 :]")
-time.sleep(1)
-print("\033[1;30m[: 1 :]")
-time.sleep(3)
+print("\033[1;35m >>>> attack has been start in: ")
+
+x = 5
+while x > 0:
+	print('\033[1;30m[: ' + x + ' :]', end='\r')
+	time.sleep(1)
+
+#time.sleep(1)
+#print("\033[1;32m[: 5 :]")
+#time.sleep(1)
+#print("\033[1;34m[: 4 :]")
+#time.sleep(1)
+#print("\033[1;31m[: 3 :]")
+#time.sleep(1)
+#print("\033[1;39m[: 2 :]")
+#time.sleep(1)
+#print("\033[1;30m[: 1 :] :] :]")
+#time.sleep(3)
+
 os.system("clear")
 time.sleep(2)
 
